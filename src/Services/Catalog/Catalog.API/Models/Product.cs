4e0a5d27da12ea576bea71a11dc7c677ceb4c3c4
Michael <mpeder75@gmail.com>
2025-01-14 20:52:59 +0100

    started CQRS and mediatR

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿
+// = default! bruges til at undertrykke en advarsel om at værdien kan være null
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+        // Navigation property
+        public List<string> Category { get; set; } = new();
+    }
+}
