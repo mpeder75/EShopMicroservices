6ecf91e8672b08f4ac833b5447010b8578f6257f
Michael <mpeder75@gmail.com>
2025-01-19 17:09:10 +0100

    MediatR & CQRS & Carter

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.API.Models;
+using SharedKernel.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+// Dto - indeholder data til at oprette et product (request object)
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+// Dto - bruges til at returnere id når product er oprettet (response object)
+public record CreateProductResult(Guid Id);
+
+// Command handler der håndtere CreateProductCommand
+internal class CreateProductCommandHandler
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command,
+        CancellationToken cancellationToken)
+    {
+        // 1. Create Product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // 2. Save Product entity to database
+
+        // 3. Return CreateProductResult result
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,41 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct;
+
+// Dto - indeholder data til at create et product (request object)
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    decimal Price
+    );
+
+// Dto - bruges til at returnere id når product er oprettet (response object)
+public record CreateProductResponse(Guid Id);
+
+// Handler - håndterer request og returnerer response
+public class CreateProductEndpoint : ICarterModule
 {
-    public class CreateProductEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                // Map request til command
+                var command = request.Adapt<CreateProductCommand>();
+
+                // Send command til MediatR
+                var result = await sender.Send(command);
+
+                // Map result til response der modtages
+                var response = result.Adapt<CreateProductResponse>();
+
+                // Returner response
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create product");
     }
+
+
 }
